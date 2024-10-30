2d720e79fe39b6fc9f875a85169d3816bf243844
Lallindo <brunolallo8@gmail.com>
2024-10-30 12:14:45 -0300

    Alteração do index e script

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,24 +7,3 @@ let imgs = document.querySelectorAll('.imgProfile');
 
 let idsArr = ["Lallindo", "marcelodepauladev", "lianzito", "Fujimiya"];
 let data = [];
-
-import {
-    Octokit
-} from "https://esm.sh/octokit";
-
-const octokit = new Octokit({
-    auth: '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'
-});
-
-async function getUserProfile(username) {
-    try {
-        const response = await octokit.rest.users.getByUsername({
-            username: username,
-        });
-        data.unshift(response.data.avatar_url);
-    } catch (error) {
-        console.error(`Erro: ${error.message}`);
-    }
-}
-
-getUserProfile('Lallindo');
